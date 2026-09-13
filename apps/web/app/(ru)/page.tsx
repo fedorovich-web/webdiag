930e35ab8ff9cf027b815cbfd66fd63246a82ee8
Roman <fedorovich.web@gmail.com>
2026-09-13 09:15:25 +0200

    refactor(web): consolidate homepage fidelity css

--- apps/web/app/(ru)/page.tsx
+++ apps/web/app/(ru)/page.tsx
@@ -1,6 +1,4 @@
 import "../home-fidelity.css";
-import "../home-polish.css";
-import "../home-approved-fidelity.css";
 import type { Metadata } from "next";
 import { HomePage } from "../../src/features/home/home-page";
 import { JsonLd } from "../../src/components/json-ld";
